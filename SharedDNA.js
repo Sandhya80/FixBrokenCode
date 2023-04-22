1905c26c27183c2ef84760255299f91abd7725d1
Sandhya80 <129001012+Sandhya80@users.noreply.github.com>
2023-04-22 18:10:25 +0100

    Initial commit

---
+++ SharedDNA.js
@@ -0,0 +1,30 @@
+//Fixing the broken code:
+//Determining familial relationship based on percentage of shared DNA
+
+const whatRelation = (percentSharedDNA) => {
+    if (percentSharedDNA === 100) {
+        return 'You are likely identical twins.'
+    }
+    else if (percentSharedDNA >= 35) {
+        return 'You are likely parent and child or full siblings.'
+    }
+    else if (percentSharedDNA >= 14) {
+        return 'You are likely grandparent and grandchild, aunt/uncle and niece/nephew, or half siblings.'
+    }
+    else if (percentSharedDNA >= 6) {
+        return 'You are likely 1st cousins.'
+    }
+    else if (percentSharedDNA >= 3) {
+        return 'You are likely 2nd cousins.'
+    }
+    else if (percentSharedDNA > 0) {
+        return 'You are likely 3rd cousins'
+    } else
+    return 'You are likely not related.'
+}
+
+console.log(whatRelation(34))
+// Should print 'You are likely grandparent and grandchild, aunt/uncle and niece/nephew, or half siblings.'
+
+console.log(whatRelation(3))
+// Should print 'You are likely 2nd cousins.'
